a2ceee376f93789cebb6a0131395fec1e9822a07
tawfikyassine <125127556+tawfikyassine@users.noreply.github.com>
2024-11-18 11:57:36 +0100

    Update send_pusher_event.R

--- send_pusher_event.R
+++ send_pusher_event.R
@@ -56,23 +56,23 @@ send_pusher_event <- function(cluster, app_id, key, secret, channel, event_name 
 }
 
 
-# Define the pusher configuration as a list
-pusher <- list(
-  cluster = "eu",
-  app_id = "app id",
-  key = "key",
-  secret = "secret",
-  channel = "channel name"
-)
+# # Define the pusher configuration as a list
+# pusher <- list(
+#   cluster = "eu",
+#   app_id = "app id",
+#   key = "key",
+#   secret = "secret",
+#   channel = "channel name"
+# )
 
-# Call the function using the pusher configuration
-send_pusher_event(
-  cluster = pusher$cluster,
-  app_id = pusher$app_id,
-  key = pusher$key,
-  secret = pusher$secret,
-  channel = pusher$channel,
-  event_name = "job_finished", # Example event name
-  list_data = list(progress = 20, details = "job finished successfully.") # Example data
-)
+# # Call the function using the pusher configuration
+# send_pusher_event(
+#   cluster = pusher$cluster,
+#   app_id = pusher$app_id,
+#   key = pusher$key,
+#   secret = pusher$secret,
+#   channel = pusher$channel,
+#   event_name = "job_finished", # Example event name
+#   list_data = list(progress = 20, details = "job finished successfully.") # Example data
+# )
   
